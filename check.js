bbcca18cf2e833134b970198826cfc18a0fe5453
Divyanshkaushal <dkaushal.enabke@gmail.com>
2018-08-02 11:51:04 +0530

    form validate

--- check.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('input[type="checkbox"]').click(function(){
-        var inputValue = $(this).attr("value");
-        $("." + inputValue).toggle();
-    });
-});
